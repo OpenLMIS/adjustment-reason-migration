0d10278318ee88c0587759be928df8f230087a70
Paweł Gesek <pgesek@soldevelo.com>
2017-08-31 11:01:57 +0200

    Release 1.0

--- util-scrips/just_run.sh
+++ util-scrips/just_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -e DATABASE_URL='jdbc:postgresql://10.222.17.221:5432/open_lmis' -e POSTGRES_USER='postgres' -e POSTGRES_PASSWORD='p@ssw0rd' -v '/tmp:/log'  openlmis/adjustment-reason-migration
+docker run -e DATABASE_URL='jdbc:postgresql://10.222.17.221:5432/open_lmis' -e POSTGRES_USER='postgres' -e POSTGRES_PASSWORD='p@ssw0rd' -v '/tmp:/log'  openlmis/adjustment-reason-migration:1.0
